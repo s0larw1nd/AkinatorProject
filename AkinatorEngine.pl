13dc74b8415c71488478eb98ce6129c4464387fc
s0larw1nd <alixess13@gmail.com>
2025-05-12 18:15:44 +0300

    Updated engine

--- AkinatorEngine.pl
+++ AkinatorEngine.pl
@@ -1,19 +1,29 @@
+:- set_prolog_flag(encoding, utf8).
+
 movie("Titanic", [basedOnTrueStory, inEnglish]).
 movie("Inception", [after2000, inEnglish]).
 movie("The Matrix", [after2000]).
 
-question(basedOnTrueStory, "Фильм основан на реальных событиях?").
-question(after2000, "Фильм снят после 2000?").
-question(inEnglish, "Фильм снят на английском?").
+question(basedOnTrueStory, "Is film based on real events?").
+question(after2000, "Was film created after 2000?").
+question(inEnglish, "Was film created in english?").
 
 check_in_features(Feature, Movie) :-
     movie(Movie,L), member(Feature, L),!.
 
-filter_by_feature(Movies, Feature, FilteredMovies) :-
-    include(check_in_features(Feature), Movies, FilteredMovies).
+check_all_features([], _).
+check_all_features([Feature|Rest], Movie) :-
+    check_in_features(Feature, Movie),
+    check_all_features(Rest, Movie).
 
+%filter_by_feature(Movies, Feature, FilteredMovies) :-
+    %include(check_in_features(Feature), Movies, FilteredMovies).
+filter_by_feature(Movies, Features, FilteredMovies) :-
+    include(check_all_features(Features), Movies, FilteredMovies).
+
+filter_by_feature_not(Movies, [], Movies).
 filter_by_feature_not(Movies, Feature, Result) :-
-    include(check_in_features(Feature), Movies, FilteredMovies),
+    include(check_all_features(Feature), Movies, FilteredMovies),
     subtract(Movies, FilteredMovies, Result).
 
 collect_tags([], []).
@@ -44,6 +54,12 @@ determine_question(Movies, Question) :-
     nth0(0,Sorted, QuestionPair),
  	Question-_=QuestionPair.
 
+determine_question_all(Questions_yes, Questions_no, Question) :-
+    all_movie_titles(Movies),
+    filter_by_feature(Movies, Questions_yes, FilteredYesMovies),
+    filter_by_feature_not(FilteredYesMovies, Questions_no, FilteredMovies),
+    determine_question(FilteredMovies, Question),!.
+
 ask_first_question :-
     all_movie_titles(Movies),
     determine_question(Movies, Question),
@@ -60,9 +76,21 @@ ask_question(Question, Movies) :-
     read(Answer),
     (
     	Answer = 1
-    ->  filter_by_feature(Movies, Question, FilteredMovies), determine_question(FilteredMovies, NewQuestion), ask_question(NewQuestion, FilteredMovies),!
-    ;   filter_by_feature_not(Movies, Question, FilteredMovies), determine_question(FilteredMovies, NewQuestion), ask_question(NewQuestion, FilteredMovies),!
+    ->  filter_by_feature(Movies, [Question], FilteredMovies), determine_question(FilteredMovies, NewQuestion), ask_question(NewQuestion, FilteredMovies),!
+    ;   filter_by_feature_not(Movies, [Question], FilteredMovies), determine_question(FilteredMovies, NewQuestion), ask_question(NewQuestion, FilteredMovies),!
     ),!.
 
 all_movie_titles(Titles) :-
     findall(Title, movie(Title, _), Titles).
+
+main :-
+    current_prolog_flag(argv, [List1Str, List2Str]),
+    read_from_chars(List1Str, List1),
+    read_from_chars(List2Str, List2),
+    determine_question_all(List1, List2, Question),
+    question(Question,QuestionLine),
+    write(Question),nl,
+    write(QuestionLine),
+    halt.
+
+:- initialization(main, main).
